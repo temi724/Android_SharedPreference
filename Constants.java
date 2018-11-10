bd883765e368b89d652fc023136026eab4f4d4f6
temi724 <tayodaniel0@gmail.com>
2018-11-10 23:10:39 +0100

    This project shows how to use sharedpreference in android

---
+++ Constants.java
@@ -0,0 +1,7 @@
+package e.danieltemtayo.aad;
+
+public class Constants {
+    public static final String FILE_NAME="myAPP";
+    public static final String KEY_NAME="Name";
+    public static final String KEY_PROFESSION="Profession";
+}
